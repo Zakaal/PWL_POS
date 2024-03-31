69adf1f4a3802c466c0ebbb18e468cea43b8925b
Zakaal <mochammadzakaro24@gmail.com>
2024-03-31 10:53:28 +0700

    Praktikum-6 jobsheet 3

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,6 +22,7 @@ Route::get('/', function () {
 
 Route::get('/level', [LevelController::class,'index']);
 Route::get('/kategori', [KategoriController::class,'index']);
+Route::get('/user', [UserController::class,'index']);
 
 // Jobsheet 5
 //Route::get('/hello', function () {
