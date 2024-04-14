87aeefa011f131c917eaca40786b6eb270cd4a49
Zakaal <mochammadzakaro24@gmail.com>
2024-04-14 19:32:56 +0700

    Laporan Jobsheet 4

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -16,7 +16,7 @@ class UserModel extends Model
         'level_id', 'username', 'nama', 'password'
     ];
 
-    /*public function level(): BelongsTo{
+    public function level(): BelongsTo{
         return $this->belongsTo(LevelModel::class);
-    }*/
+    }
 }
