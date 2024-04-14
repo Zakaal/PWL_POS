042464ee8953731847e4c0565672a6359be768fd
Zakaal <mochammadzakaro24@gmail.com>
2024-04-14 15:23:17 +0700

    Praktikum 2.1 Jobsheet 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,16 +10,20 @@ class UserController extends Controller
 {
     public function index()
     {
-        $data = [
+        /*$data = [
             'level_id' => 2,
             'username' => 'manager_tiga',
             'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
-        
-        $data['password'] = Hash::make('12345');
-        
-        UserModel::create($data);        
-        $user = UserModel::all(); //ambil semua data dari tabel m_user
+        UserModel::create($data);
+        $user = UserModel::all(); //ambil semua data dari tabel m_user*/
+        //$user = UserModel::find(1);
+        //$user = UserModel::where('level_id', 1)->first();
+        //$user = UserModel::firstWhere('level_id', 1);
+        $user = UserModel::findOr(20, ['username', 'nama'], function() {
+            abort(404);
+        });       
         return view('user', ['data'=> $user]);
     }
 }
